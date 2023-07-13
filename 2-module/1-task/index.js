cb4e6de1fa0fc587954052b780002e3d5ed73657
Elvina Filippova <101575573+elvina223@users.noreply.github.com>
2023-07-13 13:03:11 +0300

    Update index.js

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,22 +1,21 @@
-let salaries = {
+  let salaries = {
   John: 1000,
   Ann: 1600,
   Pete: 1300,
   month: 'December',
   currency: 'USD',
   isPayed: false
-}
-function sumSalary(salaries) {
-  
-  let sumSalary = 0;
-  
-  for (let key in salaries){
-  if (salaries[key] == '')return null;
-  if (typeof salaries[key] === "number" && !isNaN(salaries[key])
-      && salaries[key] != Infinity && salaries[key] != -Infinity)
-      sumSalary += salaries[key]
-     
-  }return sumSalary;
-  return null
-}
-console.log( sumSalary(salaries) );
+  } 
+  function sumSalary(salaries) {
+    let sum = 0;
+
+    for (let key in salaries) {
+    if (typeof salaries[key] == 'number' && typeof isNaN(salaries[key])
+        && isFinite(salaries[key])) {
+        sum += salaries[key];
+     };
+    
+    };
+     return sum;
+  };  
+    console.log(sumSalary(salaries))
